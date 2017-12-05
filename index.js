74012ff1c51cd64416e257c2e281ec9dd24bc411
Muhammed Nuhman <nuhman7@outlook.com>
2017-12-05 19:16:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.get("/", (req, res) => {
     //res.send(language);
 });
 
-
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on 3000...");
 });
 
